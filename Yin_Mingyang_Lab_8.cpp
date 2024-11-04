c6df543f6f21046b496f81e624ce74a38ca74f42
Mingyangyin48 <mingyangy@nycstudents.net>
2024-11-04 16:26:47 -0500

    part1

---
+++ Yin_Mingyang_Lab_8.cpp
@@ -0,0 +1,20 @@
+// Mingyang Michelle Yin
+// Lab 8
+// Nov. 4, 2024
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+    // PART 1 //
+    
+    double pointNum;
+    cout << "Please enter an floating point number: ";
+    cin >> pointNum;
+    cout << endl;
+    cout << "Square root of " << pointNum << " is: " << sqrt(pointNum) << endl;
+
+    return 0;
+}
